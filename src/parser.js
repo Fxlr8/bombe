47980226f9824569310bef1d5a778c52993726e4
Alexey Kulikov <alexeyqu@gmail.com>
2021-09-19 18:11:20 +0100

    do simple OCR for numbers

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,13 @@
 const puppeteer = require('puppeteer');
 console.log('Starting parse')
 const fs = require('fs')
+const tesseract = require("node-tesseract-ocr")
+
+const tesseract_config = {
+  lang: "rus",
+  oem: 1, // LSTM neural nets
+  psm: 6, // Assume a single uniform block of text.
+}
 
 const urls = [
     'http://www.primorsk.vybory.izbirkom.ru/region/izbirkom?action=show&root=252000008&tvd=4254005265112&vrn=100100067795849&prver=0&pronetvd=null&region=25&sub_region=25&type=242&report_mode=null',
@@ -12,13 +19,16 @@ const url = urls[0]
 const runParse = async () => {
     const browser = await puppeteer.launch();
 
-    const data = await runPage(browser, url)
+    const data = await getRawPageData(browser, url)
     fs.writeFileSync('src/output/test.json', JSON.stringify(data))
+
+    const dataOCR = await doPageDataOCR(data)
+    fs.writeFileSync('src/output/test_ocr.json', JSON.stringify(dataOCR))
 
     await browser.close();
 }
 
-const runPage = async (browser, url) => {
+const getRawPageData = async (browser, url) => {
     const page = await browser.newPage();
     await page.setViewport({ width: 2000, height: 768, deviceScaleFactor: 2 });
     await page.goto(url);    // Go website
@@ -66,6 +76,33 @@ const screenshot = async (page, selector, i) => {
     });
 
     return result
+}
+
+const doPageDataOCR = async(data) => {
+    const tasks = data.map((d, i) => {
+        return async () => {
+            value = (await get_text_from_buffer(data[i].value)).trim();
+            if (value.includes("\n")) {
+                value = value.split(/\n/)
+                data[i].value = value[0]
+                data[i].percent = value[1]
+            } else {
+                data[i].value = value
+            }
+        }
+    })
+
+    await tasks.reduce((p, task) => {
+        return p.then(() => {
+            return task()
+        })
+    }, Promise.resolve().then(r => r))
+
+    return data;
+}
+
+const get_text_from_buffer = async (buffer) => {
+    return tesseract.recognize(buffer, tesseract_config);
 }
 
 runParse()
